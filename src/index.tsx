99e49fc4fd58aa202163a96351e64b52d7ad6944
Iurii Pototskii <iuriipototskii2gmail.com>
2023-05-11 10:39:38 +0200

    init: escaperoom project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+// import { Provider } from 'react-redux';
+
+// store.dispatch(checkAuthStatus());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -8,6 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
+    {/* <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} /> */}
     <App />
+    {/* </Provider> */}
   </React.StrictMode>,
 );
